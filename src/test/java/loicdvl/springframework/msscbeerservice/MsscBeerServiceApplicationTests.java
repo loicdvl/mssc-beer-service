02978fe9999eae23f3607cb2162f7c4075706673
loicdvl <loic.devoille@hotmail.fr>
2021-01-04 11:25:34 +0100

    Initial Project Config

---
+++ src/test/java/loicdvl/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package loicdvl.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
